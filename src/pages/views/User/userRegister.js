f535e7311d179baf3f66f5a47a48e13392e7f0d1
DESKTOP-QGCV3UR\Nay Chi <nay-chishunnlae@bit-gu.com>
2024-03-13 09:14:03 +0900

    fontawesome削除

--- src/pages/views/User/userRegister.js
+++ src/pages/views/User/userRegister.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Table from "../../../components/table";
 import { useRouter } from "next/router";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown, faAngleRight, faFileLines, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import user from "../../api/user";
 
 export default function UserRegister(){

0fa0baba1fa751c42ce47ed42f24e032ae945dc0
NayChi\Nay Chi Shunn Lae <nay-chishunnlae@bit-gu.com>
2024-03-21 13:24:42 +0900

    モデルエラー修正

--- src/pages/views/Organization/list.js
+++ src/pages/views/Organization/list.js
@@ -15,13 +15,6 @@ export default function OrganizationList(){
 
     const [error, setErrors] = useState("");
 
-    //モデル
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const openModal = (event) => {
-        event.stopPropagation();
-        setIsModalOpen(true);
-    };
-
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -44,12 +37,6 @@ export default function OrganizationList(){
       const addBatch = () => {
         router.push("./batch_process")
     }
-
-    //モデル閉じる
-    const handleCloseClick = (e) => {
-        e.preventDefault();
-        onClose();
-    };
 
     const columns = React.useMemo(
         () => [
@@ -139,62 +126,60 @@ export default function OrganizationList(){
                                 <button type="button" className="btn btn-primary" onClick={addBatch} style={{ padding: "10px 40px" }}>一括処理</button>
                             </div>
                             <div className="col-6 text-end">
-                                <button type="button" onClick={ (event) => openModal(event)} className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal01" style={{ padding: "10px 40px" }}>絞り込み表示</button>&nbsp;
+                                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#Modal01" style={{ padding: "10px 40px" }}>絞り込み表示</button>&nbsp;
                                 {
-                                    isModalOpen && (
-                                        <div className="modal fade" id="Modal01" tabIndex="-1" aria-labelledby="ModalLabel01">
-                                            <div className="modal-dialog">
-                                                <div className="modal-content">
-                                                    <div className="modal-header">
-                                                        <h1 className="modal-title fs-5" id="ModalLabel01">部署絞り込み</h1>
-                                                        <button type="button" onClick={handleCloseClick} className="btn-close" data-bs-dismiss="modal" aria-label="閉じる"></button>
-                                                    </div>
-                                                    <div className="modal-body">
-                                                        <table className="table table-bordered">
-                                                            <tbody>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">部署名</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">よみ</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">コード</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">階&nbsp;層</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">上位組織</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">別&nbsp;名</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">電話番号</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                                <tr>
-                                                                    <td className="col-6 text-center align-middle bg-light py-3">備&nbsp;考</td>
-                                                                    <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
-                                                                </tr>
-                                                            </tbody>
-                                                        </table>
-                                                        <div className="modal-footer">
-                                                            <button type="button" className="btn btn-primary" style={{padding : "10px 45px"}}>絞り込み</button>
-                                                            <button type="reset" className="btn btn-secondary" data-bs-dismiss="modal" style={{padding : "10px 37px"}}>キャンセル</button>
-                                                        </div>{/* /.modal-footer  */}
-                                                    </div>
-                                                </div>{/* /.modal-content  */}
-                                            </div>{/* /.modal-dialog  */}
-                                        </div>
-                                    )
+                                    <div className="modal fade" id="Modal01" tabIndex="-1" aria-labelledby="ModalLabel01">
+                                        <div className="modal-dialog">
+                                            <div className="modal-content">
+                                                <div className="modal-header">
+                                                    <h1 className="modal-title fs-5" id="ModalLabel01">部署絞り込み</h1>
+                                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="閉じる"></button>
+                                                </div>
+                                                <div className="modal-body">
+                                                    <table className="table table-bordered">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">部署名</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">よみ</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">コード</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">階&nbsp;層</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">上位組織</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">別&nbsp;名</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">電話番号</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td className="col-6 text-center align-middle bg-light py-3">備&nbsp;考</td>
+                                                                <td className="col-6 text-center align-middle py-3"><input type="text" className="custom-input" /></td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                    <div className="modal-footer">
+                                                        <button type="button" className="btn btn-primary" style={{padding : "10px 45px"}}>絞り込み</button>
+                                                        <button type="reset" className="btn btn-secondary" data-bs-dismiss="modal" style={{padding : "10px 37px"}}>キャンセル</button>
+                                                    </div>{/* /.modal-footer  */}
+                                                </div>
+                                            </div>{/* /.modal-content  */}
+                                        </div>{/* /.modal-dialog  */}
+                                    </div>
                                 }
                                     
                             </div>
